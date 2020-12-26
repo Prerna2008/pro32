28b4561b6b1e330af33d877f3712906954210e9d
Prerna2008 <72653361+Prerna2008@users.noreply.github.com>
2020-12-26 19:52:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -114,9 +114,9 @@ function keyPressed(){
     slingshot.attach(all.body);
 }  
 }  
-async function getBackgroundImg(){
-  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-  var responseJSON = await response.json();
+ function getBackgroundImg(){
+  var response =  fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = response.json();
 
   var datetime = responseJSON.datetime;
   var hour = datetime.slice(11,13);
